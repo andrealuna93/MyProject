d8a9b1d1d6a9d09f0d50278ba4de7ee38dcd7a98
andrealuna93 <paolazambranolive@gmail.com>
2019-10-16 17:53:43 -0500

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main extends Test {
 
         setUp();
         signIn();
+        registro();
 
 
 	// write your code here
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
 
@@ -13,18 +14,23 @@ public class Test {
         //llamado al driver de Chrome
         driver = new ChromeDriver();
         //pagina a la cual se va a ingresar
-        driver.navigate().to("https://www.imdb.com/");
+        driver.navigate().to("https://co.hoteles.com/");
+        // espera para realizar la carga de la pagina
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //comprobar carga de la pagina con el boton Sign in
+        driver.findElement(By.id("header-sign-in"));
     }
 
     public static void signIn(){
         //seleccion de la opcion Sign In
-    WebElement buttonsignIn = driver.findElement(By.id("imdb-signin-link"));
-    buttonsignIn.click();
-
-        //
-
-
+        WebElement buttonsignIn = driver.findElement(By.id("header-sign-in"));
+        buttonsignIn.click();
+        //crear cuenta de nuevo usuario
     }
 
-
+    public static void registro() {
+        //seleccionar opcion de registro
+        WebElement opcionRegistro = driver.findElement(By.xpath("//a[@href='/profile/signup.html']"));
+        opcionRegistro.click();
+    }
 }
